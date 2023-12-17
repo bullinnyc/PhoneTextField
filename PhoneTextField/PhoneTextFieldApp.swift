6fff3f5a4e97721be8db55324c41fde8a5fb8fe1
bullinnyc <i8u11@icloud.com>
2023-12-17 20:22:49 +0700

    Initial Commit

---
+++ PhoneTextField/PhoneTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneTextFieldApp.swift
+//  PhoneTextField
+//
+//  Created by Dmitry Kononchuk on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhoneTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
